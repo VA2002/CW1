c9693466c4cd3508b517ebc26c05c033092ebef8
VA2002 <122964882+VA2002@users.noreply.github.com>
2023-11-18 18:27:59 +0400

    Add files via upload
    Added search bar

---
+++ lessons.js
@@ -0,0 +1,24 @@
+var classes = [
+    {
+        id: 0,
+        code: "AS101",
+        name: "History",
+        location: "Oxford",
+        price: 80,
+        teacher: "Alex Napol",
+        space: 5,
+        cartqty: 0,
+        fontawsm: `<i id="Subjecticon" class="fa-solid fa-timeline"></i>`
+      },
+      {
+        id: 1,
+        code: "AS102",
+        name: "English",
+        location: "Hendon",
+        price: 70,
+        teacher: "Will Spear",
+        space: 5,
+        cartqty: 0,
+        fontawsm: `<i id="Subjecticon" class="fa-solid fa-spell-check"></i>`
+      }
+]

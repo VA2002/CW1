7ca68787d132cfcda22bafa9a434b511f3a19efe
VA2002 <122964882+VA2002@users.noreply.github.com>
2023-11-18 18:41:56 +0400

    Add files via upload
    Can load data from lessons.js now

--- lessons.js
+++ lessons.js
@@ -1,24 +1,57 @@
 var classes = [
-    {
-        id: 0,
-        code: "AS101",
-        name: "History",
-        location: "Oxford",
-        price: 80,
-        teacher: "Alex Napol",
-        space: 5,
-        cartqty: 0,
-        fontawsm: `<i id="Subjecticon" class="fa-solid fa-timeline"></i>`
-      },
-      {
-        id: 1,
-        code: "AS102",
-        name: "English",
-        location: "Hendon",
-        price: 70,
-        teacher: "Will Spear",
-        space: 5,
-        cartqty: 0,
-        fontawsm: `<i id="Subjecticon" class="fa-solid fa-spell-check"></i>`
-      }
-]
+  {
+    id: 0,
+    code: "AS101",
+    name: "History",
+    location: "Oxford",
+    price: 80,
+    teacher: "Alex Napol",
+    space: 5,
+    cartqty: 0,
+    fontawsm: `<i id="Subjecticon" class="fa-solid fa-timeline"></i>`
+  },
+  {
+    id: 1,
+    code: "AS102",
+    name: "English",
+    location: "Hendon",
+    price: 70,
+    teacher: "Will Spear",
+    space: 5,
+    cartqty: 0,
+    fontawsm: `<i id="Subjecticon" class="fa-solid fa-spell-check"></i>`
+  },
+  {
+    id: 9,
+    code: "AS403",
+    name: "Theater",
+    location: "Birmingham",
+    price: 90,
+    teacher: "Joey Tribbiani",
+    space: 5,
+    cartqty: 0,
+    fontawsm: `<i id = "Subjecticon" class="fa-solid fa-masks-theater"></i>`
+  },
+  {
+    id: 10,
+    code: "AS421",
+    name: "Art",
+    location: "Liverpool",
+    price: 90,
+    teacher: "Baplo Cipasso",
+    space: 5,
+    cartqty: 0,
+    fontawsm: `<i id = "Subjecticon" class="fa-solid fa-palette"></i>`
+  },
+  {
+    id: 11,
+    code: "AS232",
+    name: "Swimming",
+    location: "Liverpool",
+    price: 100,
+    teacher: "George Turner",
+    space: 5,
+    cartqty: 0,
+    fontawsm: `<i id = "Subjecticon" class="fa-solid fa-person-swimming"></i>`
+  }
+];
